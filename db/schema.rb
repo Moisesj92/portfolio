4cda44d6fee93a7dbaf4986305cbe5e54024ae2f
Arsenio Jimenez <moises.jimenez92@gmail.com>
2022-07-25 23:37:36 -0400

    add home base page, post and project scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_24_234053) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_26_032932) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -53,6 +53,18 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_24_234053) do
   end
 
   create_table "admin_posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "projects", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
